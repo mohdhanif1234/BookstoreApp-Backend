0f827617826f08095328d5a4f73e1eb11091f710
mohdhanif1234 <hanif.topia@gmail.com>
2022-01-01 11:47:00 +0530

    [Hanif]Add: Added a query to create a stored procedure for inserting the values in the user table

--- DatabaseOperationsScript.sql
+++ DatabaseOperationsScript.sql
@@ -1,3 +1,4 @@
+-- Creating a user table
 Create table user_tbl(   
 
 UserId int IDENTITY(1,1) NOT NULL,   
@@ -11,3 +12,28 @@ Password varchar(20) NOT NULL,
 MobileNum bigint NOT NULL  
 
 )
+
+-- Creating a stored procedure for adding the users
+Create procedure spForAddingUsers    
+
+(   
+
+    @FullName VARCHAR(20),    
+
+    @EmailId VARCHAR(20),   
+
+    @Password VARCHAR(20),   
+
+    @MobileNum VARCHAR(6)   
+
+)   
+
+as    
+
+Begin    
+
+    Insert into users_tbl (FullName,EmailId,Password, MobileNum)    
+
+    Values (@FullName,@EmailId,@Password, @MobileNum)    
+
+End
