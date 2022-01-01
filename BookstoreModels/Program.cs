fe9f172402f8f5d82775bf2697ef277ed116b18f
mohdhanif1234 <hanif.topia@gmail.com>
2022-01-01 11:13:46 +0530

    [Hanif]Add: Created a basic folder structure for the Bookstore app

---
+++ BookstoreModels/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookstoreModels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
