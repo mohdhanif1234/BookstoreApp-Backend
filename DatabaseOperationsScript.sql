aa59ff64c99287ce1df57f29014e7d9d2a984d9f
mohdhanif1234 <hanif.topia@gmail.com>
2022-01-01 11:42:06 +0530

    [Hanif]Add: Added a query to create a user table

---
+++ DatabaseOperationsScript.sql
@@ -0,0 +1,13 @@
+Create table user_tbl(   
+
+UserId int IDENTITY(1,1) NOT NULL,   
+
+FullName varchar(20) NOT NULL,   
+
+EmailId varchar(20) NOT NULL,   
+
+Password varchar(20) NOT NULL,   
+
+MobileNum bigint NOT NULL  
+
+)
