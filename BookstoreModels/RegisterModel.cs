6de98b28ccd39f2f2dac605eafc0bacdfa675015
mohdhanif1234 <hanif.topia@gmail.com>
2022-01-02 23:09:00 +0530

    [Hanif]Add: Added a RegisterModel class

---
+++ BookstoreModels/RegisterModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BookstoreModels
+{
+    public class RegisterModel
+    {
+        public int UserId { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string EmailId { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public long MobileNum { get; set; }
+    }
+}
