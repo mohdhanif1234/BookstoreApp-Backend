74b31a895e5019fe2c789959d5a19335543937e3
mohdhanif1234 <hanif.topia@gmail.com>
2022-01-04 09:24:47 +0530

    [Hanif]Add: Created a ResetPassword model

---
+++ BookstoreModels/ResetPasswordModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BookstoreModels
+{
+    public class ResetPasswordModel
+    {
+        [Required]
+        public string EmailId { get; set; }
+        [Required]
+        public string NewPassword { get; set; }
+    }
+}
