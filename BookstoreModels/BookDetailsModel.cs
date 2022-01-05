84b304dcace970effa7418e8cea48472c4144742
mohdhanif1234 <hanif.topia@gmail.com>
2022-01-05 09:28:51 +0530

    [Hanif]Add: Added a model for book details

---
+++ BookstoreModels/BookDetailsModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BookstoreModels
+{
+    public class BookDetailsModel
+    {
+        [Key]
+        public int BookId { get; set; }
+        [Required]
+        public string BookTitle { get; set; }
+        [Required]
+        public string AuthorName { get; set; }
+        public double Rating { get; set; }
+        public int RatingCount { get; set; }
+        [Required]
+        public int OriginalPrice { get; set; }
+        [Required]
+        public int DiscountedPrice { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int BookQty { get; set; }
+        public string Image { get; set; }
+    }
+}
