244e8e78f50648bf7d87b2ac12acb6ab956b3be4
mohdhanif1234 <hanif.topia@gmail.com>
2022-01-08 23:44:04 +0530

    [Hanif]Add: Created a OrderModel class

---
+++ BookstoreModels/OrderModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookstoreModels
+{
+    public class OrderModel
+    {
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public int AddressId { get; set; }
+        public int BookId { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public string OrderDate { get; set; }
+        public BookDetailsModel bookDetailsModel { get; set; }
+    }
+}
