992522dd94178c26657ccf3e8b00fe98f75cf2f3
mohdhanif1234 <hanif.topia@gmail.com>
2022-01-13 10:35:29 +0530

    [Hanif]Add: Added a Feedback model

---
+++ BookstoreModels/FeedbackModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookstoreModels
+{
+    public class FeedbackModel
+    {
+        public int ReviewId { get; set; }
+        public string Name { get; set; }
+        public double Rating { get; set; }
+        public string Comment { get; set; }
+        public string CreatedAt { get; set; }
+        public int BookId { get; set; }
+        public int UserId { get; set; }
+    }
+}
